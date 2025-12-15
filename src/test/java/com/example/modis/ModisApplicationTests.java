0fcc64a58304ae971d5e29499983282614e89baa
Phung Khanh Duy <22130062@st.hcmuaf.edu.vn>
2025-12-15 23:14:11 +0700

    Tạo dự án Springboot

---
+++ src/test/java/com/example/modis/ModisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.modis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
