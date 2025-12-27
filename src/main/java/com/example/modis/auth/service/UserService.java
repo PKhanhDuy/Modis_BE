a860d51cd19c74e3987f73d6c46adaea875f832d
NgocDiem <22130038@st.hcmuaf.edu.vn>
2025-12-27 14:07:12 +0700

    refactor(user): consolidate UserService and resolve bean naming conflict

--- src/main/java/com/example/modis/auth/service/UserService.java
+++
@@ -1,4 +0,0 @@
-package com.example.modis.auth.service;
-
-public class UserService {
-}
--- src/main/java/com/example/modis/user/service/UserService.java
+++ src/main/java/com/example/modis/user/service/UserService.java
@@ -1,5 +1,8 @@
 package com.example.modis.user.service;
 
+import com.example.modis.auth.dto.SignUpRequest;
+import com.example.modis.user.enumm.Role;
+import com.example.modis.user.enumm.Status;
 import com.example.modis.user.model.User;
 import com.example.modis.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -24,5 +28,22 @@ public class UserService {
     public List<User> findAll() {
         log.info("Finding all users");
         return userRepository.findAll();
+    }
+
+    public User registerNewUser(SignUpRequest signUpRequest){
+        if (userRepository.findByUsername(signUpRequest.getUsername()).isPresent()) {
+            throw new RuntimeException("Tên đăng nhập đã tồn tại!");
+        }
+        User user = new User();
+        user.setUsername(signUpRequest.getUsername());
+        user.setPassword(signUpRequest.getPassword());
+        user.setFullname(signUpRequest.getFullname());
+        user.setMail(signUpRequest.getMail());
+        user.setSdt(signUpRequest.getSdt());
+
+        user.setRole(Role.USER); 
+        user.setIsActive(Status.ACTIVE);
+        user.setCreatedAt(LocalDateTime.now());
+        return userRepository.save(user);
     }
 }
