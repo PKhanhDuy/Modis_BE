f5b0cb6525a7a9b977bd9ff36b28fb7c80857faf
Phung Khanh Duy <22130062@st.hcmuaf.edu.vn>
2026-01-17 11:07:42 +0700

    fix: userId not map with DB

--- src/main/java/com/example/modis/friend/service/FriendReqService.java
+++ src/main/java/com/example/modis/friend/service/FriendReqService.java
@@ -6,6 +6,11 @@ import com.example.modis.friend.repository.FriendReqRepository;
 import com.example.modis.user.model.User;
 import com.example.modis.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
+import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -14,7 +19,8 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class FriendReqService {
-
+    private static final Logger log =
+            LoggerFactory.getLogger(FriendReqService.class);
     private final FriendReqRepository friendReqRepository;
     private final UserRepository userRepository;
 
@@ -33,7 +39,7 @@ public class FriendReqService {
             String friendId = req.getSenderId().equals(userId)
                     ? req.getReceiverId()
                     : req.getSenderId();
-
+            log.info("Da lay duoc friendId la " +friendId);
             User user = userRepository.findById(friendId)
                     .orElseThrow(() ->
                             new RuntimeException("User not found: " + friendId)
