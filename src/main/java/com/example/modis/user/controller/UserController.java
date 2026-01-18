83f66c1af3ec272cd95df95838df22c57995b2f2
vonguyenthanhdieu <22130039@st.hcmuaf.edu.vn>
2026-01-18 17:58:33 +0700

    turn off rabbit MQ

--- src/main/java/com/example/modis/user/controller/UserController.java
+++ src/main/java/com/example/modis/user/controller/UserController.java
@@ -110,7 +110,6 @@ public class UserController {
                                         "message", userId
                                 )
                         )
-                )
-        );
+                );
     }
 }
