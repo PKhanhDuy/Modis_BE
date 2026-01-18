21adc0f30aab55f5737b49151902648171cd8489
vonguyenthanhdieu <22130039@st.hcmuaf.edu.vn>
2026-01-18 14:39:28 +0700

    resovle conflict

--- src/main/java/com/example/modis/post/dto/PostDto.java
+++ src/main/java/com/example/modis/post/dto/PostDto.java
@@ -1,28 +1,21 @@
 package com.example.modis.post.dto;
-
 import com.example.modis.post.model.Receiver;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.Instant;
+import java.util.List;
 
 @Data
 @Builder
-public class PostDTO {
-    private String id;
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostDto {
     private String senderId;
-    private Receiver receiver;
+    private List<Receiver> receivers;
     private String caption;
     private String urlImage;
     private Instant created_at;
-
-    public PostDTO(String id, String senderId, Receiver receiver, String caption, String urlImage, Instant created_at) {
-        this.id = id;
-        this.senderId = senderId;
-        this.receiver = receiver;
-        this.caption = caption;
-        this.urlImage = urlImage;
-        this.created_at = created_at;
-    }
-    public PostDTO(){};
 }
--- src/main/java/com/example/modis/post/model/Receiver.java
+++ src/main/java/com/example/modis/post/model/Receiver.java
@@ -1,5 +1,6 @@
 package com.example.modis.post.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,15 +9,10 @@ import java.time.Instant;
 
 @Data
 @Builder
+@AllArgsConstructor
 @NoArgsConstructor
 public class Receiver {
     private String receiverId;
     private String icon;
     private Instant timestamp;
-
-    public Receiver(String receiverId, String icon, Instant timestamp) {
-        this.receiverId = receiverId;
-        this.icon = icon;
-        this.timestamp = timestamp;
-    }
 }
